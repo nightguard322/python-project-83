23f2039cfffab127a47c83d2e081974784b54e77
sasha <sander_@inbox.ru>
2025-06-15 18:18:33 +0300

    added ssl

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -19,6 +19,7 @@ load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY']=os.getenv('SECRET_KEY')
 DATABASE_URL = os.getenv('DATABASE_URL')
+conn = None
 
 @app.before_request
 def check_db_connection():
